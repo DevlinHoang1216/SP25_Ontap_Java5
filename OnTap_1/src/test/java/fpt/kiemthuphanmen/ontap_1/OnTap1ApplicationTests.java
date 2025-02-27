ddd1deede22d4a87e089e5a005d21143ca007773
DevlinHoang1216 <fpt.kiethv1216@gmail.com>
2025-02-27 21:26:29 +0700

    update

---
+++ OnTap_1/src/test/java/fpt/kiemthuphanmen/ontap_1/OnTap1ApplicationTests.java
@@ -0,0 +1,13 @@
+package fpt.kiemthuphanmen.ontap_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnTap1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
